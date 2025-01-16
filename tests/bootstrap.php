b19f70f36d12fec6028f70023a1c2a5ffd037c74
CyberVitexus <info@vitexsoftware.cz>
2025-01-16 19:56:30 +0100

    bootstrap cleanup

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,20 +1,21 @@
 <?php
 
+declare(strict_types=1);
+
 /**
- * This file is part of the spojenet_abraflexi-ipex package
+ * This file is part of the AbraFlexiIpex package
  *
- * https://github.com/VitexSoftware/
+ * https://github.com/Spoje-NET/abraflexi-ipex
  *
- * (c) Vítězslav Dvořák <http://vitexsoftware.com>
+ * (c) Vítězslav Dvořák <http://spojenet.cz/>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
 /**
  * @author Vítězslav Dvořák <info@vitexsoftware.cz>
  */
 // TODO: check include path
-//ini_set('include_path', ini_get('include_path'));
+// ini_set('include_path', ini_get('include_path'));
 
 // put your code here
